ea2fcb92463aa65cd94bd1fa4e8b059a1997e2f8
Pishoy <pishoyabdo@gmail.com>
2019-11-14 15:38:28 +0200

    Update package.py

--- package.py
+++ package.py
@@ -8,7 +8,7 @@ class Package(j.baseclasses.threebot_package):
         else:
             self.branch = "master"
 
-        self.goldflow_io_repo = "https://github.com/pishoy/www_goldflow_io"
+        self.goldflow_io_repo = "https://github.com/pishoy/www_goldflow_io.git"
 
     def prepare(self):
         """
